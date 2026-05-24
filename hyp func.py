ed99fce1370a8da48f77bd2c786a17fc83979451
aaron <ahaze@student.scf.edu>
2026-05-24 18:50:44 -0400

    Added Programming Exercise 1

---
+++ hyp func.py
@@ -0,0 +1,8 @@
+def main():
+        side1 = float(input('Enter length of a side: '))
+        side2 = float(input('Enter another length: '))
+        hyp = (side1 * side1 + side2 * side2) ** 0.5
+        print('Length of hyp[otenuse is: ', hyp)
+
+
+        
---
+++ hyp.py
@@ -0,0 +1,4 @@
+side1 = 5
+side2 = 12
+hyp = (side1 * side1 + side2 * side2) ** 0.5
+print(hyp)
